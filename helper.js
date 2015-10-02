23508792a6eb490512873fe4a71c8c534e50ba9f
Rics <richard_szabo@jataka.hu>
2015-10-02 09:34:54 +0200

    Helper function became one module in helper.js.

--- helper.js
+++ helper.js
@@ -2,10 +2,12 @@
 
 // -----------------------------------------------------------------------------------------------------
 // auxiliary functions
+function Helper() {
+}
 
 //
 // quasi-normal distribution standard form: [-1,1)
-function gauss_random() {
+Helper.gauss_random = function() {
     // http://www.protonfish.com/random.shtml
     return (Math.random()*2-1)+(Math.random()*2-1)+(Math.random()*2-1);
     // formerly it was the function below but that was not close to standard, there was no value above 1, below -1
@@ -13,7 +15,7 @@ function gauss_random() {
     //return ((Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random()-3)) / 3;
 }
 
-function cauchy_random() {
+Helper.cauchy_random = function() {
     // http://math.stackexchange.com/questions/484395/how-to-generate-a-cauchy-random-variable
     return Math.tan(Math.PI*(Math.random()-1.0/2));
 }
